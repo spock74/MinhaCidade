1f6c26d61148d6c1c42f9c506eb1579359831dc1
Zeh <elvano@gmail.com>
2022-05-23 20:00:58 -0300

    scrollvew horizonyal in placelist screen

--- components/places/PlaceItem.js
+++ components/places/PlaceItem.js
@@ -8,6 +8,8 @@ import {
   Text,
 } from "react-native";
 
+import { Colors } from "../../constants/Colors";
+import { ColorsRed } from "../../constants/Colors";
 
 import { getMapPreview } from "../../geoloc/getMapPreview";
 import { INITIAL_ZOOM_PREVIEW } from "../../geoloc/geoconfig";
@@ -17,43 +19,73 @@ function PlaceItem({ place, onSelect }) {
     Alert.alert("Deixa de ser preguiçoso!");
   }
 
-  
   return (
-    <ScrollView>
+    <ScrollView style={{ backgroundColor: "white" }}>
+      <View style={styles.headerInfo}>
+        <Text style={{ color: "white" }}>
+          Endereço: {place.location.address}
+        </Text>
+        <Text style={{ color: "white" }}>
+          Data de Criação: {new Date(place.date).toLocaleDateString()}{" "}
+          {new Date(place.date).toLocaleTimeString()}
+        </Text>
+        <Text style={{ color: "white" }}>Drescrição: {place.description}</Text>
+        <Text style={{ color: "white" }}> </Text>
+        <Text style={{ color: "white" }}>Latitude: {place.location.lat}</Text>
+        <Text style={{ color: "white" }}>Longitude: {place.location.lon}</Text>
+      </View>
+
       <View style={styles.container}>
         <View style={styles.viewLinhaImagem}>
-          <Pressable onPress={f}>
-            <Image style={styles.imageCam} source={{ uri: place.image.uri }} />
-          </Pressable>
-          <Pressable>
-            <Image
-              style={styles.miniMap}
-              source={{
-                uri: getMapPreview(
-                  place.location.lat,
-                  place.location.lon,
-                  INITIAL_ZOOM_PREVIEW
-                ),
-              }}
-            />
-          </Pressable>
-        </View>
-        <View style={styles.viewLinhaText}>
-          <Text style={{ color: "white" }}>
-            Endereço: {place.location.address}
-          </Text>
-          <Text style={{ color: "white" }}>
-            Data de Criação: {new Date(place.date).toLocaleDateString()}{" "}
-            {new Date(place.date).toLocaleTimeString()}
-          </Text>
-          <Text style={{ color: "white" }}>
-            Drescrição: {place.description}
-          </Text>
-          <Text style={{ color: "white" }}> </Text>
-          <Text style={{ color: "white" }}>Latitude: {place.location.lat}</Text>
-          <Text style={{ color: "white" }}>
-            Longitude: {place.location.lon}
-          </Text>
+          <ScrollView horizontal={true}>
+            <Pressable onPress={f}>
+              <Image
+                style={styles.imageCam}
+                source={{ uri: place.image.uri }}
+              />
+            </Pressable>
+            <Pressable style={styles.imageCam} onPress={f}>
+              <Image
+                style={styles.imageCam}
+                source={{ uri: place.image.uri }}
+              />
+            </Pressable>
+            <Pressable style={styles.imageCam} onPress={f}>
+              <Image
+                style={styles.imageCam}
+                source={{ uri: place.image.uri }}
+              />
+            </Pressable>
+            <Pressable style={styles.imageCam} onPress={f}>
+              <Image
+                style={styles.imageCam}
+                source={{ uri: place.image.uri }}
+              />
+            </Pressable>
+          </ScrollView>
+          <ScrollView horizontal={true}>
+            <Pressable>
+              <Image
+                style={styles.miniMap}
+                source={{
+                  uri: getMapPreview(
+                    place.location.lat,
+                    place.location.lon,
+                    INITIAL_ZOOM_PREVIEW
+                  ),
+                }}
+              />
+            </Pressable>
+            <Pressable onPress={f}>
+              <Text style={styles.imageCam}>1</Text>
+            </Pressable>
+            <Pressable onPress={f}>
+              <Text style={styles.imageCam}>2</Text>
+            </Pressable>
+            <Pressable onPress={f}>
+              <Text style={styles.imageCam}>3</Text>
+            </Pressable>
+          </ScrollView>
         </View>
       </View>
     </ScrollView>
@@ -63,11 +95,12 @@ function PlaceItem({ place, onSelect }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 4,
-    backgroundColor: "#004d40",
+    marginRight: 4,
+    marginLeft: 4,
+    marginTop: 0,
+    backgroundColor: "white",
     borderRadius: 10,
-    borderColor: "#388e3c",
-    borderWidth: 1,
+    borderWidth: 0,
   },
   viewLinhaImagem: {
     flexDirection: "column",
@@ -82,28 +115,34 @@ const styles = StyleSheet.create({
   },
 
   imageCam: {
+    margin: 6,
     width: 360,
     height: 250,
     borderRadius: 10,
+    alignItems: "center",
+    alignContent: "center",
+    justifyContent: "center",
   },
 
   miniMap: {
     width: 360,
     height: 250,
+    margin: 4,
     borderRadius: 10,
     overflow: "hidden",
+    borderWidth: 1,
+    borderColor: "#CCC",
   },
 
-  viewLinhaText: {
-    width: 360,
-
-    backgroundColor: "#004d40",
+  headerInfo: {
+    backgroundColor: ColorsRed.primary700,
     borderRadius: 10,
     color: "white",
-    marginTop: 2,
+    marginTop: 12,
+    marginHorizontal: 6,
     marginBottom: 4,
-    marginHorizontal: 4,
-    padding: 4,
+    padding: 6,
+    elevation: 4,
   },
 
   pressed: {},
